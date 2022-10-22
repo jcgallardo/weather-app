dd282faff68a1a9c3169d5f421f3f6190803c440
Juan Carlos Gallardo <jcgallardomorales@gmail.com>
2022-10-22 13:00:57 +0200

    refactor names

---
+++ src/components/CityList/CityList.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import CityInfo from '../CityInfo/CityInfo'
+import Weather from '../Weather/Weather'
+
+const renderCityAndCountry = cityAndCountry => {
+    const { city, country } = cityAndCountry;
+    return (
+        <li key={ city }>
+            <CityInfo city={ city } country={ country }  />
+            <Weather state='cloudy' temperature={ 10 }  />
+        </li>
+    )
+}
+
+const CityList = ({ cities }) => {
+    return cities?.length && (
+        <ul>
+            {
+                cities.map(renderCityAndCountry)
+            }
+        </ul>
+    )
+}
+
+CityList.propTypes = {
+    cities: PropTypes.arrayOf(PropTypes.shape({
+        city: PropTypes.string.isRequired,
+        country: PropTypes.string.isRequired
+    })).isRequired
+}
+
+export default CityList
