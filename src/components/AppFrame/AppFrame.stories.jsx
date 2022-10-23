7284d0fd045840b22b02e81c582c4fcf88c68bdb
Juan Carlos Gallardo <jcgallardomorales@gmail.com>
2022-10-23 13:27:40 +0200

    AppFrame component

---
+++ src/components/AppFrame/AppFrame.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AppFrame from '.'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+export default {
+    title: 'AppFrame',
+    component: AppFrame
+}
+
+export const AppFrameExample = () => <Router><AppFrame /></Router>
