98625f2bb15b5e48c910b0757311fa29680be168
Juan Carlos Gallardo <jcgallardomorales@gmail.com>
2022-10-23 18:12:38 +0200

    useEffect, useState and Vanta animation

--- src/components/WelcomeScreen/WelcomeScreen.jsx
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
+import Clouds from 'vanta/dist/vanta.clouds.min'
+import * as THREE from 'three'
 
 const WelcomeScreen = ({
     children
 }) => {
-  return (
-    <div>
-        WelcomeScreen
-    </div>
-  )
+    const myRef = useRef(null);
+    const [vanta, setVanta] = useState(null);
+
+    // didMount
+    useEffect(()=> {
+        if (!vanta){
+            setVanta(
+                Clouds({
+                    THREE,
+                    el: myRef.current
+                })
+            )
+            console.log('Vanta initialized!')
+        }
+    }, [vanta]);
+
+    return (
+        <div ref={ myRef }>
+            WelcomeScreen
+        </div>
+    )
 }
 
 WelcomeScreen.propTypes = {
