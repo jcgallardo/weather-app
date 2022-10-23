5584fe31474923e050a9c0e80deeba05a105374c
Juan Carlos Gallardo <jcgallardomorales@gmail.com>
2022-10-23 17:51:55 +0200

    WelcomeScreen skeleton

---
+++ src/components/WelcomeScreen/WelcomeScreen.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const WelcomeScreen = ({
+    children
+}) => {
+  return (
+    <div>
+        WelcomeScreen
+    </div>
+  )
+}
+
+WelcomeScreen.propTypes = {
+    children: PropTypes.node
+}
+
+export default WelcomeScreen
