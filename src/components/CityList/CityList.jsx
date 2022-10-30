d57eca29a1953c061e93d18a27a98eef17b6f46a
Juan Carlos Gallardo <jcgallardomorales@gmail.com>
2022-10-30 18:51:21 +0100

    fetching weather cities

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+import axios from 'axios'
 import CityInfo from '../CityInfo'
 import Weather from '../Weather'
 import { Grid, List, ListItem } from '@material-ui/core'
 
 // Currying (Purificación) - función que devuelve otra función
-const renderCityAndCountry = onClickCity => cityAndCountry => {
+const renderCityAndCountry = onClickCity => (cityAndCountry, weather) => {
     const { city, country } = cityAndCountry;
     return (
         <ListItem
@@ -21,7 +22,11 @@ const renderCityAndCountry = onClickCity => cityAndCountry => {
                     <CityInfo city={ city } country={ country }  />
                 </Grid>
                 <Grid item md={3} xs={12}>
-                    <Weather state='cloudy' temperature={ 10 }  />
+                    {
+                        weather
+                            ? <Weather state={ weather.state } temperature={ weather.temperature.toFixed(0) }  />
+                            : "No hay datos"
+                    }
                 </Grid>
             </Grid>
         </ListItem>
@@ -29,10 +34,42 @@ const renderCityAndCountry = onClickCity => cityAndCountry => {
 }
 
 const CityList = ({ cities, onClickCity }) => {
+    const [allWeather, setAllWeather] = useState({});
+
+    useEffect(()=>{
+        const setWeather = (city, country) => axios.get(process.env.REACT_APP_WEATHER_URL.replace('{city}', city))
+            .then(response => {
+                const { data: { main: { temp } } } = response;
+                const propName = `${city}-${country}`;
+                const temperature = temp - 273; // La respuesta da la temperatura en grados Kelvin
+                const state = 'sunny';
+                const propValue = { temperature, state } 
+
+                // ¡NOTA!: Para asegurar que estamos cogiendo el estado anterior, es necesario indicar una función al setter.
+                // De la siguiente manera no obtendríamos el estado anterior en cada iteracción.
+                // setAllWeather({
+                //     ...allWeather,
+                //     [propName]: propValue
+                // })
+
+                // De la siguiente manera nos asguramos que allWeather contiene la información actualizada
+                setAllWeather(allWeather => ({
+                    ...allWeather,
+                    [propName]: propValue
+                }));
+            });
+        cities.forEach(({ city, country }) => {
+            setWeather(city, country);
+        });
+    }, [cities])
+
     return cities?.length ? (
         <List>
             {
-                cities.map(cityAndCountry => renderCityAndCountry(onClickCity)(cityAndCountry))
+                cities.map(cityAndCountry => renderCityAndCountry(onClickCity)(
+                    cityAndCountry,
+                    allWeather[`${cityAndCountry.city}-${cityAndCountry.country}`]
+                ))
             }
         </List>
     ) : null;
