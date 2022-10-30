db1acb00ed1325d63edeaaa800c54351142c3761
Juan Carlos Gallardo <jcgallardomorales@gmail.com>
2022-10-30 21:13:39 +0100

    mui skeleton

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Grid, Typography } from '@material-ui/core'
+import Skeleton from '@material-ui/lab/Skeleton'
 import IconState from '../IconState';
 
 const Weather = ({ state, temperature }) => {
   return (
     <Grid container item direction='row' justifyContent='center' alignItems='center' spacing={1}>
       <IconState state={ state } contextOptions={{ size: '6em' }}  />
-      <Typography display='inline' variant='h2'>{ temperature }</Typography> 
+      {
+        temperature 
+          ? <Typography display='inline' variant='h2'>{ temperature }</Typography> 
+          : <Skeleton variant="rect" animation="wave" height={96} width={96} />
+      }
     </Grid>
   )
 }
 
 Weather.propTypes = {
-  state: PropTypes.string.isRequired,
-  temperature: PropTypes.number.isRequired
+  state: PropTypes.string,
+  temperature: PropTypes.number
 }
 
 export default Weather
