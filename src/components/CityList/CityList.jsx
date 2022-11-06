db6b578faa14b4140f9e54c0055940692659f509
Juan Carlos Gallardo <jcgallardomorales@gmail.com>
2022-11-06 20:37:12 +0100

    custom hook useCityList

--- src/components/CityList/CityList.jsx
+++ src/components/CityList/CityList.jsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import axios from 'axios'
-import convertUnits from 'convert-units'
 import CityInfo from '../CityInfo'
 import Weather from '../Weather'
 import { Grid, List, ListItem } from '@material-ui/core'
 import { Alert } from '@material-ui/lab'
-
-const getCityCode = (city, countryCode) => `${city}-${countryCode}`;
+import useCityList from '../../hooks/useCityList'
+import { getCityCode } from '../../utils/utils'
 
 // Currying (Purificación) - función que devuelve otra función
 const renderCityAndCountry = onClickCity => (cityAndCountry, weather) => {
@@ -34,60 +32,7 @@ const renderCityAndCountry = onClickCity => (cityAndCountry, weather) => {
 };
 
 const CityList = ({ cities, onClickCity }) => {
-    const [allWeather, setAllWeather] = useState({});
-    const [error, setError] = useState(null);
-
-    useEffect(()=>{
-        const setWeather = async (city, countryCode) => {
-            try {
-                const response = await axios.get(
-                    process.env.REACT_APP_WEATHER_URL
-                        .replace('{city}', city)
-                        .replace('{countryCode}', countryCode)
-                )
-                
-                const {
-                    data: { 
-                        main: { temp },
-                        weather
-                    }
-                } = response;
-
-                const propName = getCityCode(city, countryCode);
-                const temperature = Number(convertUnits(temp).from('K').to('C').toFixed(0));
-                const propValue = { 
-                    temperature,
-                    state: weather[0]?.main?.toLowerCase()
-                };
-
-                // ¡NOTA!: Para asegurar que estamos cogiendo el estado anterior, es necesario indicar una función al setter.
-                // De la siguiente manera no obtendríamos el estado anterior en cada iteracción.
-                // setAllWeather({
-                //     ...allWeather,
-                //     [propName]: propValue
-                // })
-
-                // De la siguiente manera nos asguramos que allWeather contiene la información actualizada
-                setAllWeather(allWeather => ({
-                    ...allWeather,
-                    [propName]: propValue
-                }));
-            } catch (error) {
-                if (error.response) { // Errores que nos response el server
-                    setError("Ha ocurrido un error en el servidor del clima");
-                } else if (error.request) { // Errores que suceden por no llegar al server
-                    setError("Verifique la conexión a internet");
-                } else {
-                    setError("Ha ocurrido un error inesperado");
-                }
-            }
-        }
-
-        cities.forEach(({ city, country, countryCode }) => {
-            setWeather(city, countryCode);
-        });
-
-    }, [cities])
+    const { allWeather, error, setError } = useCityList(cities);
 
     return  (
         <div>
